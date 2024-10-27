1aef8abacfb2ab4156f493ec95370cbcad140ad0
cahayoyo <cahayoyo@gmail.com>
2024-10-27 20:59:14 +0700

    making table index.jsx

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -12,10 +12,13 @@ export default function Index({ auth, projects }) {
       }
     >
       <Head title="Projects" />
+
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-            <div className="p-6 text-gray-900 dark:text-gray-100"><pre>{JSON.stringify(projects,undefined,2)}</pre></div>
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+              
+            </div>
           </div>
         </div>
       </div>
